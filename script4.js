c4ae329030d9c7406802c2b8ebcefab86f69525a
francisGolden <doria5@live.it>
2022-04-04 20:59:30 +0200

    fixed delete one book feature and changed design

--- script4.js
+++ script4.js
@@ -1,48 +1,43 @@
-// initialize myLibrary array
+
 myLibrary = []
 
-// select container with variable
+
 libs = document.querySelector("#libs-container")
 
-// declare book constructor
+
 function Book(author, title, pages) {
     this.author = author
     this.title = title
     this.pages = pages
 }
 
-// short for console.log
+
 log = console.log
 
-// select submit button with variable
+
 const submit = document.querySelector("#submit") 
 
-// add event listener to submit button
+
 submit.addEventListener("click", function(e) {
-    // prevent default behaviour of form submission
+    
     e.preventDefault();
 
-    
-
-
-    // construct new book with values gathered from inputs
     let nuovo = new Book(
         form.author.value,
         form.title.value,
         form.pages.value
         )
     
-    // insert the new book to myLibrary
+   
     myLibrary.push(nuovo)
     log(nuovo)
     log(myLibrary)
     
-    // create a div so that every book has a div to be
-    // into that can be flexed or 'grided'
+    
     let contain = document.createElement("div")
     
     
-    // add class name to the div
+   
     contain.classList.add("shelf")
 
     for (i=0; i<myLibrary.length; i++) {
@@ -51,51 +46,47 @@ submit.addEventListener("click", function(e) {
 
 
 
-    // insert the div element in libs container
+   
     libs.insertAdjacentElement("beforeend", contain)
     
     
 
-    // for every property in book
+    
     for (property in nuovo) {
-        // insert in the container div other divs
-        // with the book properties
         contain.innerHTML += `<div class="property">${nuovo[property]}</div> `
-        
     }
     
-    // for (i=0; i<myLibrary.length; i++) {
-    //     contain.setAttribute("id", `${i}`)
-    // }
+    deleteAll = document.querySelector("#deleteAll")
 
-    contain.innerHTML += `<button class="property" id="deleteOne">Delete</button>`
-    let deleteOne = document.querySelector("#deleteOne")
-    for (i = 0; i<myLibrary.length; i++) {
-        deleteOne.setAttribute("id", `b${i}`)
-        deleteOne.addEventListener("click", function() {
-            deleteRef = i
-            d = deleteRef.toString()
-            let toDelete = document.getElementById(d)
-            log(deleteRef)
-            log(toDelete)
-            log(i)
-            
+    deleteAll.addEventListener("click", function(e) {
+        myLibrary = []
+        e.preventDefault()
+        shelves = document.querySelectorAll(".shelf")
+        log(shelves.length)
+
+        shelves.forEach(book => {
+          book.remove();
         })
-    }
 
-
-    // for every element in myLibrary, create as many rows
+    })
+  
+   
+    contain.innerHTML += `<button class="property" id="deleteOne">Delete</button>`
+    let deleteOne = document.querySelectorAll("#deleteOne")
+    
+      
+    deleteOne.forEach(book => {
+      book.addEventListener("click", function() {
+        book.parentNode.remove()
+      })
+    })    
+        
+     
+    
+  
     libs.style.gridTemplateRows = `repeat(${myLibrary.length}, 1fr)`;
 
     // for every element in the book, create as many columns
     contain.style.gridTemplateColumns = `repeat(${nuovo.length}, 1fr)`;
     
-})
-
-// declare delete all button
-deleteAll = document.querySelector("#deleteAll")
-
-// add event to delete all button
-deleteAll.addEventListener("click", function() {
-    myLibrary = []
 })
