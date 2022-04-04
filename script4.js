da9f009b89d2b45ceb383f32072e6c91d2eb64fd
Francesco Doria <doria5@live.it>
2022-04-04 21:20:03 +0200

    added comments for clarity

--- script4.js
+++ script4.js
@@ -4,7 +4,7 @@ myLibrary = []
 
 libs = document.querySelector("#libs-container")
 
-
+//object constructor
 function Book(author, title, pages) {
     this.author = author
     this.title = title
@@ -21,41 +21,37 @@ const submit = document.querySelector("#submit")
 submit.addEventListener("click", function(e) {
     
     e.preventDefault();
-
+    
+    //create new book with form values
     let nuovo = new Book(
         form.author.value,
         form.title.value,
         form.pages.value
         )
     
-   
+    //add book to array
     myLibrary.push(nuovo)
     log(nuovo)
     log(myLibrary)
     
-    
+    //create new div that will contain book infos
     let contain = document.createElement("div")
-    
-    
-   
+  
     contain.classList.add("shelf")
 
     for (i=0; i<myLibrary.length; i++) {
         contain.setAttribute("id", `a${i}`)
     }
-
-
-
-   
+    
+    //insert contain in libs
     libs.insertAdjacentElement("beforeend", contain)
     
-    
-
-    
+    //for every property in new book, inject the property to book container HTML
     for (property in nuovo) {
         contain.innerHTML += `<div class="property">${nuovo[property]}</div> `
     }
     
+    //select "delete all" button
     deleteAll = document.querySelector("#deleteAll")
 
     deleteAll.addEventListener("click", function(e) {
@@ -63,7 +59,8 @@ submit.addEventListener("click", function(e) {
         e.preventDefault()
         shelves = document.querySelectorAll(".shelf")
         log(shelves.length)
-
+        
+        //delete every book in all shelves
         shelves.forEach(book => {
           book.remove();
         })
@@ -74,7 +71,7 @@ submit.addEventListener("click", function(e) {
     contain.innerHTML += `<button class="property" id="deleteOne">Delete</button>`
     let deleteOne = document.querySelectorAll("#deleteOne")
     
-      
+    //add an event listener to every deleteOne button and make it delete its parentNode 
     deleteOne.forEach(book => {
       book.addEventListener("click", function() {
         book.parentNode.remove()
